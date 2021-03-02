47968d33540c0720b589e88d71c10ebcb6382a9c
craigdsouza <craigds022@gmail.com>
2021-03-02 23:16:54 +0530

    changed ui to allow selection of years for lulc

--- ruralwater.py
+++ ruralwater.py
@@ -44,10 +44,10 @@ class RuralWaterClass:
       filename, _filter = QFileDialog.getOpenFileName(                  # use file dialog to get filename 
         self.dlg, "Select shape file ","", '*.shp')
       self.dlg.lineEdit.setText(filename)
-      self.addLayer()
+      self.add_boundary_layer()
       
-    def addLayer(self):
-      print("called addLayer")
+    def add_boundary_layer(self):
+      print("called add_boundary_layer")
       self.filename = self.dlg.lineEdit.text()
       print(self.filename)
       self.layername = os.path.basename(self.filename).split(".")[0]
@@ -60,9 +60,9 @@ class RuralWaterClass:
       symbol = single_symbol_renderer.symbol()
       symbol.symbolLayer(0).setBrushStyle(Qt.BrushStyle(Qt.NoBrush))
       
-      self.getStates()
+      self.get_states()
       
-    def getStates(self):
+    def get_states(self):
       print("called get States")
       li = self.vlayer.uniqueValues(1)
       if qgis.core.NULL in li:
@@ -70,7 +70,7 @@ class RuralWaterClass:
       self.dlg.comboBox.clear()
       self.dlg.comboBox.addItems(li)
 
-    def getDistricts(self):
+    def get_districts(self):
       print("called get Districts")
       self.state = self.dlg.comboBox.currentText()
       stateFilter = "\"stat_name\"='" + self.state + "'"
@@ -88,7 +88,7 @@ class RuralWaterClass:
       self.dlg.comboBox_2.addItems(set(li))
       
       
-    def getBlocks(self):
+    def get_blocks(self):
       print("called get Block")
       self.dist = self.dlg.comboBox_2.currentText()
       distFilter = "\"dist_name\"='" + self.dist + "'"
@@ -109,7 +109,7 @@ class RuralWaterClass:
 #      rainStats = distRain.January.mean()
 #      self.dlg.lineEdit_3.setText("Mean January rainfall 2004-2011 in {dist} is:".format(dist="Thanjavur") + str(rainStats))
       
-    def getVillages(self):
+    def get_villages(self):
       print("called get Villages")
       self.block = self.dlg.comboBox_3.currentText()
       blockFilter = "\"sdst_name\"='" + self.block + "'"
@@ -126,7 +126,7 @@ class RuralWaterClass:
       self.dlg.comboBox_4.clear()
       self.dlg.comboBox_4.addItems(set(li))
     
-    def zoomVillage(self):
+    def zoom_village(self):
       print("called zoom to village")
       vlg = self.dlg.comboBox_4.currentText()
       vlgstring = "\"vlgcd2011\"='"+vlg+"'"
@@ -145,20 +145,31 @@ class RuralWaterClass:
       rainStats = distRain.January.mean()
       self.dlg.lineEdit_3.clear()
       self.dlg.lineEdit_3.setText("Mean January rainfall 2004-2011 in {dist} is:".format(dist=self.dist) + str(rainStats))
-      
-    def run(self):
-      image = ee.Image('users/gsnshinde/UPSCAPE_LULCFInal/LULC_2020_21Class')
+    
+    def populate_lulc_choices(self):
+      li = ["2010_11","2011_12","2012_13","2013_14","2014_15","2015_16","2016_17","2017_18","2018_19","2019_20","2020_21"]
+      self.dlg.comboBox_5.clear()
+      self.dlg.comboBox_5.addItems(set(li))
+        
+    def add_lulc_image(self):
+      chosenLULCYr = self.dlg.comboBox_5.currentText()
+      geeAssetString = 'users/gsnshinde/UPSCAPE_LULCFInal/LULC_' + chosenLULCYr + 'Class'
+      image = ee.Image(geeAssetString)
       paletteLULC = ['02451E','06FC6D','FC0D06','28B505','750776','C713A9','C713A9','C713A9','E27FF9','E27FF9','E27FF9','765904','765904','765904','EAB828','EAB828','EAB828','092CEE','09EECB','Grey','Black']
       Map.addLayer(image, {'palette': paletteLULC, 'min': 0, 'max': 20}, 'lulc 2020_21', True)
+        
+    def run(self):
       imd_path = "https://storage.googleapis.com/imd-precipitation-historical-districts/IMD_Precipitation_TN_2004_2011.csv"
       self.df = pd.read_csv(imd_path)
       print(type(self.df))  
       self.dlg = RuralWaterDialog()
       self.dlg.show()
       
+      self.populate_lulc_choices()
       self.dlg.pushButton.clicked.connect(self.select_output_file)      # Select shape file 
-      self.dlg.comboBox.currentTextChanged.connect(self.getDistricts)
-      self.dlg.comboBox_2.currentTextChanged.connect(self.getBlocks)
-      self.dlg.comboBox_3.currentTextChanged.connect(self.getVillages)
-      self.dlg.comboBox_4.currentTextChanged.connect(self.zoomVillage)
+      self.dlg.pushButton_2.clicked.connect(self.add_lulc_image)
+      self.dlg.comboBox.currentTextChanged.connect(self.get_districts)
+      self.dlg.comboBox_2.currentTextChanged.connect(self.get_blocks)
+      self.dlg.comboBox_3.currentTextChanged.connect(self.get_villages)
+      self.dlg.comboBox_4.currentTextChanged.connect(self.zoom_village)
 #      self.dlg.lineEdit_3.textChanged.connect(self.getRainStats)
