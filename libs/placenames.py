3988a14e8334b48c9fef462dbe455b57aaf7231e
Balakumaran <balakumaran247@gmail.com>
2022-09-27 16:08:03 +0530

    updated: placenames

--- libs/placenames.py
+++ libs/placenames.py
@@ -1,5 +1,165 @@
 ST_names = {
+    'AN': ['Andaman & Nicobar Island'],
+    'AP': ['Andhra Pradesh'],
+    'AR': ['Arunachal Pradesh', 'Arunanchal Pradesh'],
+    'AS': ['Assam'],
+    'BR': ['Bihar'],
+    'CG': ['Chhattisgarh'],
+    'CH': ['Chandigarh'],
+    'DD': ['Daman & Diu'],
+    'DL': ['NCT of Delhi', 'Delhi'],
+    'DN': ['Dadara & Nagar Havelli', 'Dadra and Nagar Hav', 'DH'],
+    'GA': ['Goa'],
+    'GJ': ['Gujarat'],
+    'HP': ['Himachal Pradesh'],
+    'HR': ['Haryana'],
+    'JH': ['Jharkhand'],
+    'JK': ['Jammu & Kashmir'],
+    'KA': ['Karnataka'],
+    'KL': ['Kerala'],
+    'LA': ['Ladakh'],
+    'LK': ['Lakshadweep'],
+    'ME': ['Meghalaya'],
+    'MH': ['Maharashtra'],
+    'MI': ['Mizoram'],
+    'MN': ['Manipur'],
+    'MP': ['Madhya Pradesh'],
+    'NG': ['Nagaland'],
+    'OD': ['Odisha'],
+    'PB': ['Punjab'],
+    'PU': ['Puducherry', 'Pondicherry', 'PY'],
+    'RJ': ['Rajasthan'],
+    'SK': ['Sikkim'],
+    'TG': ['Telangana'],
+    'TN': ['Tamil Nadu', 'Tamilnadu'],
+    'TR': ['Tripura'],
+    'UP': ['Uttar Pradesh'],
+    'UT': ['Uttarakhand'],
+    'WB': ['West Bengal']
 }
 
 DT_names = {
+    'KA': {
+        'CHIK': ['Chikbalapur', 'Chikballapur', 'Chikkaballapura'],
+        'BELL': ['Bellary', 'Ballari'],
+        'BANG': ['Bangalore Rural', 'Bengaluru Rural'],
+        'BENG': ['Bengaluru Urban', 'Bangalore Urban', 'Bangalore', 'Bengaluru'],
+        'CHIM': ['Chikkmagaluru', 'Chikmagalur'],
+        'DAKS': ['Dakshin Kannad', 'Dakshina Kannada'],
+        'BELG': ['Belgaum', 'Belagavi'],
+        'GULB': ['Kalaburagi', 'Gulbarga'],
+        'SHIV': ['Shimoga', 'Shivamogga'],
+        'BIJA': ['Bijapur', 'Vijayapura'],
+        'CHAM': ['Chamarajanagar', 'Chamrajnagar'],
+        'UTTA': ['Uttar kannad','Uttara kannada']
+    },
+    'MH': {
+        'AHMA': ['Ahmednagar', 'Ahmadnagar'],
+        'BEED ': ['Bid', 'Bheed', 'Beed'],
+        'BULD': ['Buldana', 'Bhuldana', 'Buldhana'],
+        'RAIG': ['Raigad', 'Raigarh'],
+        'MUMB': ['Mumbai', 'Mumbai Suburban'],
+        'GARC': ['Garhchiroli', 'Gadchiroli'],
+        'GOND': ['Gondiya', 'Gondia']  
+    },
+    'TN': {
+        'CHEN': ['Chengalpattu', 'Chinglepet'],
+        'KANC': ['Kanchipuram', 'Kancheepuram'],
+        'KANN': ['Kanyakumari', 'Kanniyakumari'],
+        'THOO': ['Thoothukkudi', 'Thoothukudi'],
+        'VILU': ['Viluppuram', 'Villupuram'],
+        'VIRU': ['Virudunagar', 'Virudhunagar'],
+        'NAGA': ['Nagappattinam', 'Nagapattinnam']
+    },
+    'AP': {
+        'YSR': ['Y.s.r', 'Kadapa', 'Cuddapah', 'YSR District Kadapa'],
+        'SRI' : ['Nellore', 'Sri Potti Sriramulu Nellore', 'SPSR Nellore'],
+        'VISA': ['Visakhapatanam', 'Visakhapatnam']
+    },
+    'GJ': {
+        'AHME': ['Ahmedabad', 'Ahmadabad'],
+        'DANG': ['Dang', 'The Dangs']
+    },
+    'UP': {
+        'KASG': ['Kasganj', 'Kanshiram Nagar'],
+        'KUSH': ['Kushi Nagar', 'Kushinagar'],
+        'AMRO': ['J.P.Nagar', 'Jyotiba Phule Nagar', 'Amroha'],
+        'BHAD': ['Bhadohi', 'Sant Ravidas Nagar'],
+        'MAHA': ['Maharajganj', 'Mahraganj'],
+        'RAEB': ['Rae Bareli', 'Raebareli'],
+        'SHRA': ['Shrawasti', 'Shravasti'],
+        'SIDH': ['Sidharthnagar', 'Sidharth Nagar']
+    },
+    'WB': {
+        'COOC': ['Coochbehar', 'Koch Bihar'],
+        'DARJ': ['Darjeeling', 'Darjiling'],
+        'HOWR': ['Howrah', 'Haora'],
+        'HOOG': ['Hooghly', 'Hugli'],
+        'NORT': ['North 24 Paraganas', 'North Twenty Four Parganas'],
+        'SOUT': ['South 24 Paraganas', 'South Twenty Four Parganas'],
+        'PURU': ['Puruliya', 'Purulia']
+    },
+    'UT': {
+        'HARI': ['Haridwar', 'Hardwar']
+    },
+    'TG': {
+        'MAHB': ['Mahabubnagar', 'Mahaboobnagar', 'Mahbubnagar']
+    },
+    'AS': {
+        'MORI': ['Morigaon', 'Marigaon'],
+        'DIMA': ['Dima Hasao', 'North Cachar Hills']
+    },
+    'CG': {
+        'DANT': ['Dantewada', 'Dakshin Bastar Dantewada'],
+        'KANK': ['Kanker', 'Uttar Bastar Kanker']
+    },
+    'DN': {
+        'DADR': ['Dadra & Nagar Haveli', 'Dadra And Nagar Haveli']
+    },
+    'HP': {
+        'LAHU': ['Lahul & Spiti', 'Lhul And Spiti']
+    },
+    'JK': {
+        'BUDG': ['Budgam', 'Badgam'],
+        'BAND': ['Bandipore', 'Bandipora'],
+        'BARA': ['Baramula', 'Baramulla'],
+        'RAJA': ['Rajouri', 'Rajauri'],
+        'SHOP': ['Shupiyan', 'Shopian']
+    },
+    'JH': {
+        'KHUN': ['Khunti', 'Khuti'],
+        'KODA': ['Kodarma', 'Koderma'],
+        'SARA': ['Saraikela-kharsawan', 'Sareikela And Kharsawan ', 'Seraikela-Kharsawan', 'Saraikela-Kharswana', 'Saraikela kharsawan'],
+        'PURB': ['Purbi Singhbhum', 'East Singhbhum'],
+        'PASH': ['Pashchimi Singhbhum', 'Paschim Singhbhum', 'West Singhbhum'],
+        'PAKU': ['Pakaur','Pakur'],
+        'SAHI': ['Sahibganj','Sahebganj']
+    },
+    'MP': {
+        'NARS': ['Narsimhapur', 'Narsinghpur'],
+        'SING': ['Singrauli', 'Singrouli']
+    },
+    'ME': {
+        'RIBH': ['Ribhoi', 'Ri Bhoi']
+    },
+    'OD': {
+        'BALA': ['Baleshwar', 'Balasore'],
+        'BAUD': ['Baudh', 'Boudh'],
+        'JAJP': ['Jajpur', 'Jajapur'],
+        'KHOR': ['Khordha', 'Khurda', 'Khorda'],
+        'DEBA': ['Debagarh', 'Deogarh'],
+        'NABA': ['Nabarangapur', 'Nabarangpur'],
+        'SUBA': ['Sonapur', 'Sonepur', 'Subarnapur']
+    },
+    'PU': {
+        'PUDU': ['Puducherry', 'Pondicherry']
+    },
+    'PB': {
+        'FIRO': ['Firozpur', 'Firozepur'],
+        'SAHI': ['Sahibzada Ajit Singh Nagar', 'S A S Nagar']
+    },
+    'AN': {
+        'NICO': ['Nicobars', 'Nicobar'],
+        'NORT': ['North & Middle Andaman', 'North And Middle Andaman']
+    },
 }
